d7b71ec32541226908b5f878bfacfb798b2ba9a5
liquid_amber <liquid.amber.ja@gmail.com>
2014-07-14 16:43:02 +0900

    Add 3rd example, simple linked list and move

---
+++ p003_simple_list_and_move.rs
@@ -0,0 +1,22 @@
+enum SimpleList<T> {
+    Cons(T, Box<SimpleList<T>>),
+    Nil,
+}
+
+fn length<T>(xs: &SimpleList<T>) -> int {
+    match xs {
+        &Cons(_, ref ys) => 1 + length(*ys),
+        &Nil => 0,
+    }
+}
+
+fn main() {
+    let mut xs = box Nil;
+    xs = box Cons(3, xs);
+    xs = box Cons(2, xs);
+    xs = box Cons(1, xs);
+    let ys = xs;
+    println!("{}", length(ys)); // OK
+    // xs is moved. It will be compiler error.
+    // println!("{}", length(xs)); // NG!
+}
