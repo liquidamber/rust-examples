1ece75e7c84eaa9eed9b9aadf5bdd60149cb4499
liquid_amber <liquid.amber.ja@gmail.com>
2014-07-15 12:21:42 +0900

    Add haddock to Haskell source

--- b001_fib_seq.hs
+++ b001_fib_seq.hs
@@ -1,3 +1,15 @@
+{- |
+Module      : Main
+Description : sequential fibonacci benchmark.
+Copyright   : (c) liquid_amber, 2014
+License     : MIT
+Maintainer  : liquid.amber.ja@gmaill.com
+Stability   : experimental
+Portability : non-portable
+
+This example runs fibonacci implemented as unboxed int function.
+-}
+
 {-# LANGUAGE MagicHash #-}
 
 import GHC.Prim
@@ -9,6 +21,7 @@ fib n | n < 2 = n
 fib n = fib (n - 1) + fib (n - 2)
 -}
 
+-- | Calculate fibonacci n.
 fib :: Int# -> Int#
 fib n | n <# 2# = n
 fib n = fib (n -# 1#) +# fib (n -# 2#)
