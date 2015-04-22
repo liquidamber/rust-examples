65768b974ad094c3fdd7f456b2352ceeb9b557b9
liquid_amber <liquid.amber.ja@gmail.com>
2015-04-22 15:45:43 +0900

    Use primitive integer

--- b001_fib_seq.hs
+++ b001_fib_seq.hs
@@ -12,7 +12,6 @@ This example runs fibonacci implemented as unboxed int function.
 
 {-# LANGUAGE MagicHash #-}
 
-import GHC.Prim
 import GHC.Exts
 
 {-
@@ -22,10 +21,20 @@ fib n = fib (n - 1) + fib (n - 2)
 -}
 
 -- | Calculate fibonacci n.
+-- | n must positive integer!!
+fib :: Word# -> Word#
+fib 0## = 0##
+fib 1## = 1##
+fib n = fib (n `minusWord#` 1##) `plusWord#` fib (n `minusWord#` 2##)
+
+{-
 fib :: Int# -> Int#
-fib n | n <# 2# = n
+fib 0# = 0#
+fib 1# = 1#
 fib n = fib (n -# 1#) +# fib (n -# 2#)
+-}
 
 main :: IO ()
-main = print $ I# (fib 40#)
+-- main = print $ I# (fib 40#)
+main = print $ W# (fib 40##)
 -- main = print $ fib 40
